546c7eeeff08fcb88671db33c804b5d66024bd19
shad0whead <mfcoxjr@gmail.com>
2025-04-07 21:05:27 -0400

    Update AuthContext.tsx

--- frontend/src/components/auth/contexts/AuthContext.tsx
+++ frontend/src/components/auth/contexts/AuthContext.tsx
@@ -7,8 +7,7 @@ import {
   onAuthStateChanged,
   sendPasswordResetEmail
 } from 'firebase/auth';
-import { auth } from '../utils/firebase';
-
+import { auth } from '../../../../utils/firebase';
 interface AuthContextType {
   currentUser: User | null;
   loading: boolean;
